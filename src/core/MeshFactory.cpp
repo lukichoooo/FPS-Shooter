f78e6462eff7b9964a8f91660119fa2cef44f5af
lukichoooo <khundadzeluka702@gmail.com>
2026-01-07 16:45:37 +0400

    MeshFactory squares and pyramids

--- src/core/MeshFactory.cpp
+++ src/core/MeshFactory.cpp
@@ -12,10 +12,10 @@ MeshFactory::MeshFactory(const MeshFactoryConfig &config)
 Mesh *MeshFactory::buildSquare()
 {
     Vertex vertices[] = {
-        {{-1.0, 1.0, 0.0}},
+        {{0.0, 1.0, 0.0}},
         {{1.0, 1.0, 0.0}},
-        {{1.0, -1.0, 0.0}},
-        {{-1.0, -1.0, 0.0}},
+        {{1.0, 0.0, 0.0}},
+        {{0.0, 0.0, 0.0}},
     };
 
     GLuint indices[] = {
@@ -26,7 +26,7 @@ Mesh *MeshFactory::buildSquare()
     meshes = std::span<Mesh>(reinterpret_cast<Mesh *>(storage), index + 1);
 
 #ifdef DEBUG
-    spdlog::info("Built Square at index{}", index);
+    spdlog::info("Built Square stored at index{}", index);
 #endif
 
     index++;
@@ -36,23 +36,29 @@ Mesh *MeshFactory::buildSquare()
 Mesh *MeshFactory::buildPyramid()
 {
     Vertex vertices[] = {
-        {{-1.0, 0.0, 1.0}},
-        {{1.0, 1.0, 1.0}},
-        {{1.0, 0.0, -1.0}},
-        {{0.0, 1.0, 0.0}},
+        {{0.0, 0.0, 0.0}},
+        {{1.0, 0.0, 0.0}},
+        {{1.0, 0.0, 1.0}},
+        {{0.0, 0.0, 1.0}},
+
+        {{0.5, 1.0, 0.5}},
     };
 
+
     GLuint indices[] = {
-        0, 1, 2,
-        0, 1, 3,
-        0, 2, 3,
-        2, 1, 3};
+        0, 1, 2, 0, 2, 3,
+
+        0, 1, 4,
+        1, 2, 4,
+        2, 3, 4,
+        3, 0, 4};
+
 
     Mesh *m = new (&storage[index]) Mesh(vertices, indices, Colors::Silver);
     meshes = std::span<Mesh>(reinterpret_cast<Mesh *>(storage), index + 1);
 
 #ifdef DEBUG
-    spdlog::info("Built Pyramid at index{}", index);
+    spdlog::info("Built Pyramid stored at index{}", index);
 #endif
 
     index++;
